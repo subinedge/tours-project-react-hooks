1b0b9b66a5d428e1f353f1a72d7b275e0c3a448c
Subin Sudhakaran-Project SAPPER::Accenture <acc032@GMMCHENNAI.COM>
2020-10-28 19:03:17 +0530

    tours project completed

---
+++ setup/src/App.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+const url = 'https://course-api.netlify.app/api/react-tours-project'
+
+function App() {
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
+  const fetchTours = async () => {
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+      setLoading(false)
+      setTours(tours)
+      console.log(tours)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+
+  if (tours.length === 0) {
+    return (
+      <div className="title" style={{ margin: '4rem 0' }}>
+        <h2>No Tours left</h2>
+        <button className="btn" onClick={fetchTours}>
+          Fetch Tours
+        </button>
+      </div>
+    )
+  } else {
+    return (
+      <main>
+        <Tours tours={tours} removeTour={removeTour} />
+      </main>
+    )
+  }
+}
+
+export default App
---
+++ setup/src/Tour.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const Tour = ({ id, image, info, name, price, removeTour }) => {
+  const [readMore, setReadMore] = useState(false)
+
+  const handleMoreText = () => {
+    setReadMore(!readMore)
+  }
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">{price}</h4>
+        </div>
+
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={handleMoreText}>
+            {readMore ? `Show Less` : `Read More`}
+          </button>
+        </p>
+        <button className="delete-btn" onClick={() => removeTour(id)}>
+          Not Interested
+        </button>
+      </footer>
+    </article>
+  )
+}
+
+export default Tour
